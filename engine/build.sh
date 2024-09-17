1d6f7ea12e8ea9fb16221a9c5cede8c9fe999c05
Jonas <tuyendev@gmail.com>
2024-09-17 10:38:34 +0700

    update docker compose build

--- engine/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker buildx create --use --name py3-builder --node py3-builder0
-docker buildx build --platform linux/arm64,linux/amd64 --tag ifelsedotone/asktube-engine:latest . --push
--- web/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker buildx create --use --name bun-builder --node bun-builder0
-docker buildx build --platform linux/arm64,linux/amd64 --tag ifelsedotone/asktube-web:latest . --push
