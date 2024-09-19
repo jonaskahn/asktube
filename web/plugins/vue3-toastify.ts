44788c6d553cfb7c83ea9122101eb7e096825288
jonaskahn <tuyendev@gmail.com>
2024-09-19 23:15:09 +0700

    update style toast

--- web/plugins/vue3-toastify.ts
+++ web/plugins/vue3-toastify.ts
@@ -5,7 +5,9 @@ export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(Vue3Toastify, {
     position: toast.POSITION.TOP_CENTER,
     autoClose: 2000,
-    toastStyle: "z-index:1000000000",
+    toastStyle: {
+      "z-index": 1001,
+    },
   });
 
   return {
